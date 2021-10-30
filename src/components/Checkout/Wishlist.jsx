b81e3c2ee6fe28a5b2b70dc3b708841276434156
HecAllende <hectorallende@gmail.com>
2021-10-30 15:13:56 -0300

    Controlando

--- src/components/Checkout/Wishlist.jsx
+++ src/components/Checkout/Wishlist.jsx
@@ -9,40 +9,49 @@ const Wishlist = ({ el }) => {
     const { addCarrito, isInCart } = useContext(CartContext)
 
     return (
+            
 
         <NavDropdown.Item className="count--button">
             <div className="row shadow bg-body rounded px-1 count--button">
+                
+           
+             
                 <div className="col-12 d-flex align-items-stretch count--button">
 
-                    <div className="col-4 d-flex align-items-center count--button">
-                        <img src={el.img1} alt={el.name} width="50rem" height="50rem" className="" />
+                    <div className="col-4 d-flex align-items-center count--button mt-2">
+                        <img src={el.img1} alt={el.name} width="60rem" height="50rem" className="" />
                     </div>
 
                     <div className="col-6 d-flex flex-column mx-1 text-center lh-1 py-1">
 
-                        <p className="fw-light">{el.name}</p>
+                        
+                   
                         <p className="fw-light fw-bold text-muted">${el.price}</p>
 
                     </div>
-                    <div className="d-flex flex-column col-2 count--button">
 
-                        <button className=" count--button text-danger d-block ms-1" onClick={() => removeItem(el.id)}><box-icon name='window-close' color="grey" size="sm"></box-icon></button>
+                    <div className="d-flex flex-column col-1 count--button">
+
+                        <button className=" count--button text-danger d-block" onClick={() => removeItem(el.id)}><box-icon name='window-close' color="grey" size="sm"></box-icon></button>
 
                     </div>
 
 
                 </div>
 
+                <p className="fw-light fs-6 mt-1">{el.name}</p>
+
 
                 {isInCart(el.id) ?
                     <>
-
-                        <p className="text-muted fs-sm fw-light">Producto ya agregado al carrito</p>
+                        <p className="fw-light fs-6">{el.name}</p>
+                        <p className="text-muted fs-sm fw-light mt-1">Producto ya agregado al carrito</p>
                        
 
                     </>
                     :
                     <>
+              
                         <button
                             onClick={() => {
                                 addCarrito(el);
@@ -54,11 +63,13 @@ const Wishlist = ({ el }) => {
 
 
                 }
+          
 
               
             </div>
 
         </NavDropdown.Item>
+
 
      );
 }
